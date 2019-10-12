8ac009abc9306ebeb71fc7641264ccb9d7f239b9
Whitney Sam <whitneysamwallace@gmail.com>
2019-10-11 17:22:18 -0700

    deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://noteful-client.whitneysamwallace.now.sh',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://noteful@localhost/noteful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/noteful_test'
 };
