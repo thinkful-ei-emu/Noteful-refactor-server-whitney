33dc9ca9bbe2ddb6ad47ca7d6de567f8cf741f12
Whitney Sam <whitneysamwallace@gmail.com>
2019-10-11 17:02:32 -0700

    deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://noteful@localhost/noteful',
-  DB_TEST_URL: process.env.DB_TEST_URL || 'postgresql://noteful@localhost/noteful_test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://noteful@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://noteful@localhost/noteful_test'
 };
